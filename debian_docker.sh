edddff10c99567e416969fb8e65a1274143584d7
Kam <kam@playgen.com>
2017-04-28 11:24:04 +0100

    Add files via upload

---
+++ debian_docker.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+sudo apt-get -y update && sudo apt-get -y upgrade
+
+sudo apt-get install \
+     apt-transport-https \
+     ca-certificates \
+     curl \
+     gnupg2 \
+     software-properties-common
+
+$ curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+
+sudo apt-get install docker-ce
+
+sudo docker run hello-world
